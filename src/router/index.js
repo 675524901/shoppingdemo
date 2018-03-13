a74eaa42a44e7e678075614e187d748a8d4c38a1
unknown <13815207752@163.com>
2018-03-13 15:35:10 +0800

    router中必须有/的路径

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/views/Login'
+import Hello from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: Hello
+    },
+    {
+      path: '/login',
       name: 'Login',
       component: Login
     }
